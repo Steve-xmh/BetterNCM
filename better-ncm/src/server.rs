0960223aa3bc120d595858c47c3685f1ae924ffb
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2022-09-09 04:12:19 +0800

    调整了代码位置

--- better-ncm/src/server.rs
+++ better-ncm/src/server.rs
@@ -5,7 +5,9 @@ use std::{
     thread::JoinHandle,
 };
 
-trait TcpStreamExt: Write {
+mod handler;
+
+pub(super) trait TcpStreamExt: Write {
     fn write_ok_response(&mut self) {
         let _ = self.write_all(b"HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n");
     }
@@ -73,7 +75,7 @@ fn handle_stream(mut stream: TcpStream) {
         if let Some(body_position) = body_position {
             let content_length = content_length.unwrap_or(0);
             if req_data.len() >= body_position + content_length {
-                handle_request(
+                handler::handle_request(
                     &mut stream,
                     &method,
                     &path,
@@ -108,7 +110,7 @@ fn handle_stream(mut stream: TcpStream) {
                         let content_length = content_length.unwrap_or(0);
                         if content_length == 0 {
                             let body_position = body_position.unwrap_or(0);
-                            handle_request(
+                            handler::handle_request(
                                 &mut stream,
                                 &method,
                                 &path,
@@ -125,76 +127,6 @@ fn handle_stream(mut stream: TcpStream) {
                 }
             }
         }
-    }
-}
-
-fn handle_request(stream: &mut TcpStream, method: &str, path: &str, body: &[u8]) {
-    if let Some(path) = path.strip_prefix("/api") {
-        return match path {
-            "/app/version" => {
-                stream.write_text_response("1.0.0");
-            }
-            "/utils/show_console" => {
-                unsafe {
-                    let hwnd = winapi::um::wincon::GetConsoleWindow();
-                    if hwnd.is_null() {
-                        winapi::um::consoleapi::AllocConsole();
-                    } else {
-                        winapi::um::winuser::ShowWindow(hwnd, winapi::um::winuser::SW_SHOW);
-                    }
-                }
-                stream.write_ok_response();
-            }
-            "/utils/hide_console" => {
-                unsafe {
-                    let hwnd = winapi::um::wincon::GetConsoleWindow();
-                    winapi::um::wincon::FreeConsole();
-                    if !hwnd.is_null() {
-                        winapi::um::winuser::ShowWindow(hwnd, winapi::um::winuser::SW_HIDE);
-                    }
-                }
-                stream.write_ok_response();
-            }
-            other => {
-                if let Some(dir_path) = other.strip_prefix("/fs/read_dir?path=") {
-                    if let Ok(dir_path) = urlencoding::decode(dir_path) {
-                        if let Ok(read_dir) = std::fs::read_dir(dir_path.into_owned()) {
-                            let mut result = Vec::with_capacity(64);
-
-                            for entry in read_dir.flatten() {
-                                result.push(tinyjson::JsonValue::String(
-                                    entry.path().to_string_lossy().to_string(),
-                                ));
-                            }
-
-                            let result = tinyjson::JsonValue::Array(result);
-                            if let Ok(result) = result.stringify() {
-                                stream.write_json_response(&result);
-                                return;
-                            }
-                        }
-                    }
-                } else if let Some(file_path) = other.strip_prefix("/fs/read_file_text?path=") {
-                    if let Ok(file_path) = urlencoding::decode(file_path) {
-                        if let Ok(data) = std::fs::read_to_string(file_path.into_owned()) {
-                            stream.write_text_response(&data);
-                            return;
-                        }
-                    }
-                } else if let Some(file_path) = other.strip_prefix("/fs/write_file_text?path=") {
-                    if method == "POST" {
-                        if let Ok(file_path) = urlencoding::decode(file_path) {
-                            if std::fs::write(file_path.into_owned(), body).is_ok() {
-                                stream.write_ok_response();
-                                return;
-                            }
-                        }
-                    }
-                }
-                println!("[WARN] Unknown path {}", other);
-                stream.write_not_found_response();
-            }
-        };
     }
 }
 
